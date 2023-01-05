49a537116e5f7a53a04581bb403954191e497fe9
Sebastiao Matusse <matussevalete@gmail.com>
2023-01-05 14:55:20 -0700

    complete the date format activy and formated the code.

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,3 +1,20 @@
-const options = {weekdays: "long", day: "numeric", month: "short", year: "numeric" };
-document.getElementById("currentdate").textContent = new Date().toLocaleString("en-US", options)
-document.getElementById("arrivalDate").textContent = new Date().toLocaleString("en-Us", options)
+const firstOptions = {
+  weekdays: "short",
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+};
+const options = {
+  weekdays: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+document.getElementById("currentdate").textContent = new Date().toLocaleString(
+  "en-US",
+  firstOptions
+);
+document.getElementById("arrivalDate").textContent = new Date().toLocaleString(
+  "en-Us",
+  options
+);
