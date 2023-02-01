c31e6845cb8be447cd80711f8c269e3fcaaee63b
Sebastiao Matusse <matussevalete@gmail.com>
2023-02-01 04:22:45 -0700

    added javaScript

---
+++ lesson3/scripts/currentdate.js
@@ -0,0 +1,17 @@
+const firstOptions = {
+  weekdays: "short",
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+};
+const options = {
+  weekdays: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+document.getElementById("currentdate").textContent = new Date().toLocaleString(
+  "en-US",
+  firstOptions
+);
+
