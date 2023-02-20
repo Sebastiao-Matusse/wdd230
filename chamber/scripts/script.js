a4fbbc21f2b38a3884c88359e2ce9f4dc1a4a7de
Sebastiao Matusse <matussevalete@gmail.com>
2023-02-20 13:21:55 +0200

    enhanced the banner element

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -25,16 +25,14 @@ date[1].textContent = new Date().toLocaleString("en-UK", firstOptions);
 
 let day = new Date().getDay();
 
-const heroImageDivTag = document.querySelector("#hero-image");
-const sectionContainer = document.createElement("section");
+const bannerContainer = document.querySelector(".banner");
 const bannerElement = document.createElement("h2");
-sectionContainer.setAttribute("class", "banner");
+
 
 if (day === 1 || day === 2) {
-  heroImageDivTag.appendChild(sectionContainer);
   bannerElement.textContent =
     "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-  sectionContainer.appendChild(bannerElement);
+  bannerContainer.appendChild(bannerElement);
 } else {
-  sectionContainer.remove();
+  bannerContainer.remove();
 }
