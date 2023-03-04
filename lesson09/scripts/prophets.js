6ba616e3ef779e949eb977abacdb4400b18902e9
Sebastiao Matusse <matussevalete@gmail.com>
2023-03-04 06:24:49 -0700

    started the lesson09

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,41 @@
+const url =
+  "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+
+async function getProphetData(url) {
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    // console.table(data.prophets); // note that we reference the prophet array of the data object given the structure of the json file
+    displayProphets(data.prophets);
+  }
+}
+
+const displayProphets = (prophets) => {
+  const cards = document.querySelector("divs.cards");
+
+  prophets.forEach((prophet) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2");
+    let portrait = document.createElement("img");
+
+    // Build the h2 content out to show the prophet's full name - finish the template string
+    h2.textContent = `${prophet.name} ____________`;
+
+    // Build the image portrait by setting all the relevant attribute
+    portrait.setAttribute("src", prophet.imageurl);
+    portrait.setAttribute("alt", `portrait of ${prophet.name} ____________`);
+    portrait.setAttribute("loading", "lazy");
+    portrait.setAttribute("width", "340");
+    portrait.setAttribute("height", "440");
+
+    // Append the section(card) with the created elements
+    card.appendChild(h2);
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  });
+};
+
+// getProphetData();
