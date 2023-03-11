5669695bc2a6ea195a9c4d2f36f8ad55597f2623
Sebastiao Matusse <matussevalete@gmail.com>
2023-03-11 08:44:04 -0700

    added style for the list view and removed unused declarations

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -135,6 +135,7 @@ const displayCompanies = (companies) => {
     address.textContent = `${company.address}`;
     phones.textContent = `${company.phones}`;
     website.setAttribute("href", company.Website);
+    website.textContent = "Website ↗";
 
     portrait.setAttribute("src", company.img);
     portrait.setAttribute("loading", "lazy");
@@ -151,3 +152,22 @@ const displayCompanies = (companies) => {
     // console.log(cards);
   });
 };
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("div.cards");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+  // example using arrow function
+  display.classList.add("grid");
+  display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+  display.classList.add("list");
+  display.classList.remove("grid");
+}
