6b55ff984333468692cb23727739ecd29f3ab086
Sebastiao Matusse <matussevalete@gmail.com>
2023-04-06 14:06:51 -0600

    days in weather section displaying correctly

--- bountiful-foods/scripts/weather.js
+++ bountiful-foods/scripts/weather.js
@@ -31,8 +31,8 @@ async function apiFetch() {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      // console.log(data);
-      displayResults(data, 0, 1, 2, 3);
+      console.log(data);
+      displayResults(data, 0, 5, 13, 21);
       // displayResults(data, 1);
     } else {
       throw Error(await response.text());
@@ -44,16 +44,22 @@ async function apiFetch() {
 
 apiFetch();
 
-function displayResults(weatherData, index, index_one, indexTwo, indexThree) {
-  currentTemp.innerHTML = `<strong>${weatherData.list[index].main.temp.toFixed(
-    0
-  )}</strong>`;
-  humidity.innerHTML = `<strong>${weatherData.list[index].main.humidity.toFixed(
-    0
-  )}</strong>`;
+function displayResults(
+  weatherData,
+  index_one,
+  index_two,
+  index_three,
+  index_four
+) {
+  currentTemp.innerHTML = `<strong>${weatherData.list[
+    index_one
+  ].main.temp.toFixed(0)}</strong>`;
+  humidity.innerHTML = `<strong>${weatherData.list[
+    index_one
+  ].main.humidity.toFixed(0)}</strong>`;
 
-  const iconsrc = `https://openweathermap.org/img/w/${weatherData.list[index].weather[0].icon}.png`;
-  const desc = weatherData.list[index].weather[0].description;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.list[index_one].weather[0].icon}.png`;
+  const desc = weatherData.list[index_one].weather[0].description;
 
   weatherIcon.setAttribute("src", iconsrc);
   weatherIcon.setAttribute("alt", desc);
@@ -62,14 +68,14 @@ function displayResults(weatherData, index, index_one, indexTwo, indexThree) {
   // DAY TWO
 
   dayTwoTemp.innerHTML = `<strong>${weatherData.list[
-    index_one
+    index_two
   ].main.temp.toFixed(0)}</strong>`;
   humidityTwo.innerHTML = `<strong>${weatherData.list[
-    index_one
+    index_two
   ].main.humidity.toFixed(0)}</strong>`;
 
-  const iconsrcTwo = `https://openweathermap.org/img/w/${weatherData.list[index_one].weather[0].icon}.png`;
-  const descTwo = weatherData.list[index_one].weather[0].description;
+  const iconsrcTwo = `https://openweathermap.org/img/w/${weatherData.list[index_two].weather[0].icon}.png`;
+  const descTwo = weatherData.list[index_two].weather[0].description;
 
   dayTwoWeatherIcon.setAttribute("src", iconsrcTwo);
   dayTwoWeatherIcon.setAttribute("alt", descTwo);
@@ -77,14 +83,14 @@ function displayResults(weatherData, index, index_one, indexTwo, indexThree) {
 
   // DAY THREE
   dayThreeTemp.innerHTML = `<strong>${weatherData.list[
-    indexTwo
+    index_three
   ].main.temp.toFixed(0)}</strong>`;
   // humidityThree.innerHTML = `<strong>${weatherData.list[
-  //   indexTwo
+  //   index_three
   // ].main.humidity.toFixed(0)}</strong>`;
 
-  const iconsrcThree = `https://openweathermap.org/img/w/${weatherData.list[indexTwo].weather[0].icon}.png`;
-  const descThree = weatherData.list[indexTwo].weather[0].description;
+  const iconsrcThree = `https://openweathermap.org/img/w/${weatherData.list[index_three].weather[0].icon}.png`;
+  const descThree = weatherData.list[index_three].weather[0].description;
 
   dayThreeWeatherIcon.setAttribute("src", iconsrcThree);
   dayThreeWeatherIcon.setAttribute("alt", descThree);
@@ -92,16 +98,69 @@ function displayResults(weatherData, index, index_one, indexTwo, indexThree) {
 
   // DAY FOUR
   dayFourTemp.innerHTML = `<strong>${weatherData.list[
-    indexThree
+    index_four
   ].main.temp.toFixed(0)}</strong>`;
   // humidityThree.innerHTML = `<strong>${weatherData.list[
-  //   indexThree
+  //   index_four
   // ].main.humidity.toFixed(0)}</strong>`;
 
-  const iconsrcFour = `https://openweathermap.org/img/w/${weatherData.list[indexThree].weather[0].icon}.png`;
-  const descFour = weatherData.list[indexThree].weather[0].description;
+  const iconsrcFour = `https://openweathermap.org/img/w/${weatherData.list[index_four].weather[0].icon}.png`;
+  const descFour = weatherData.list[index_four].weather[0].description;
 
   dayFourWeatherIcon.setAttribute("src", iconsrcFour);
   dayFourWeatherIcon.setAttribute("alt", descFour);
   captionDesc.textContent = descTwo.toUpperCase();
+
+  let dayOf = weatherData.list[index_three].dt_txt;
+  // console.log(typeof dayOf);
+
+  const displayDayThree = document.querySelector(".day-three");
+  const displayDayFour = document.querySelector(".day-four");
+
+  let dateThree = new Date(`${weatherData.list[index_three].dt_txt}`);
+  let dateFour = new Date(`${weatherData.list[index_four].dt_txt}`);
+  let dayFour = dateFour.getDay();
+  let dayThree = dateThree.getDay();
+  let weekday;
+
+  if (dayThree == 0) {
+    weekday = "Sunday";
+  } else if (dayThree == 1) {
+    weekday = "Monday";
+  } else if (dayThree == 2) {
+    weekday = "Tuesday";
+  } else if (dayThree == 3) {
+    weekday = "Wednesday";
+  } else if (dayThree == 4) {
+    weekday = "Thursday";
+  } else if (dayThree == 5) {
+    weekday = "Friday";
+  } else if (dayThree === 6) {
+    weekday = "Saturday";
+  } else {
+    weekday = "Day 3";
+  }
+  displayDayThree.innerHTML = weekday;
+
+  console.log(`First display: ${weekday}`);
+
+  if (dayFour == 0) {
+    weekday = "Sunday";
+  } else if (dayFour == 1) {
+    weekday = "Monday";
+  } else if (dayFour == 2) {
+    weekday = "Tuesday";
+  } else if (dayFour == 3) {
+    weekday = "Wednesday";
+  } else if (dayFour == 4) {
+    weekday = "Thursday";
+  } else if (dayFour == 5) {
+    weekday = "Friday";
+  } else if (dayFour === 6) {
+    weekday = "Saturday";
+  } else {
+    weekday = "Day 4";
+  }
+  displayDayFour.innerHTML = weekday;
+  console.log(`Second display: ${weekday}`);
 }
